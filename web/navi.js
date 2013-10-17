e126e6bda9aff8170e3e1703361e93d6a8099735
Gabriel Vieira <gluisvieira@gmail.com>
2013-10-17 19:33:19 -0300

    Painfully learning JS...

--- web/navi.js
+++ web/navi.js
@@ -1,42 +1,51 @@
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/ambiance");
 editor.getSession().setMode("ace/mode/javascript");
-editor.setFontSize(16);
+editor.setFontSize(18);
 editor.getSession().setTabSize(4);
 editor.getSession().setUseSoftTabs(true);
 
 var terminal = ace.edit("terminal");
-terminal.setTheme("ace/theme/ambiance");
+terminal.setTheme("ace/theme/terminal");
 terminal.getSession().setMode("ace/mode/javascript");
-//terminal.renderer.setShowGutter(false);
+terminal.renderer.setShowGutter(false);
 terminal.setHighlightActiveLine(false);
 terminal.setReadOnly(true);
 terminal.setFontSize(16);
 
-// easter egg: mion_rocks => play music
+if(typeof(String.prototype.trim) === "undefined")
+{
+    String.prototype.trim = function() 
+    {
+        return String(this).replace(/^\s+|\s+$/g, '');
+    };
+}
+
+function printInput (s) { if (s) {s.trim();} terminal.insert(s + "\n"); };
+function printOutput (s) { if (s) {s.trim();} terminal.insert("=> " + s + "\n"); };
+
+function execute () {
+  "use strict";
+  var text = editor.session.getTextRange(editor.getSelectionRange());
+  printInput(text);
+  try {
+    var result = window.eval(text);
+    printOutput(result ? result.toSource() : result);
+  } catch(err) {
+    if (err instanceof SyntaxError) {
+      printOutput("[!] The syntax is incorrect.");
+    } else {
+      printOutput("[!] Error: " + err.message);
+    }
+  }
+}
+
+//easter egg: mion_rocks => play music
 editor.commands.addCommand({
-    name: 'eval',
+    name: 'execute',
     bindKey: {win: 'Ctrl-E',  mac: 'Command-E'},
-    exec: function (editor) {
-      var text = editor.session.getTextRange(editor.getSelectionRange());
-      terminal.insert(">> " + text + "\n");
-      try {
-        var result = eval(text + ";");
-        terminal.insert("=> " + eval(text) + "\n");
-      } catch(err) {
-        if (err instanceof SyntaxError) {
-          terminal.insert("[!] The syntax is incorrect.");
-        }
-      }
-    },
+    exec: execute,
     readOnly: true // false if this command should not apply in readOnly mode
 });
 
-function output (s) {
-  terminal.setValue(s);
-};
-
-function naviPlay () {
-  lisp.interpret(editor.getValue());
-};
-
+document.getElementById("btn-eval").onclick = execute;
